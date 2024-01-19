e66a2923d53288d681540c68cb9c4901285b9763
syedali042 <salizahid75@gmail.com>
2024-01-19 14:11:03 +0500

    Initial commit from Create Next App

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: 'public',
+  fallbacks: {
+    document: '/offline',
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withPWA(nextConfig);
